07e7e349ba5ddc051fadc339c8e9f4fb59e1ec34
Codinglone <codinglone@gmail.com>
2023-11-21 19:55:13 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program {
+    public static void Main(string[] args){
+        Console.WriteLine("Enter three numbers respectively below:");
+        int numOne = Convert.ToInt32(Console.ReadLine());
+        int numTwo = Convert.ToInt32(Console.ReadLine());
+        int numThree = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("The average of 3 numbers is: " + (numOne + numTwo + numThree) / 3);
+    }
+}
